53e7a4fbffddb3b75de4b96bfe604df01bbc3f11
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-14 17:12:11 -0500

    add function print char

--- funcs3.c
+++ funcs3.c
@@ -79,17 +79,18 @@ void _mod(stack_t **stack, unsigned int line_number)
  */
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = NULL;
+	int num = 0;
 
 	(void)line_number;
-	if (!*stack || !(*stack)->next)
+	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	current = *stack;
-	if (0 <= current->n || current->n <= 126)
-		putchar(current->n);
+
+	num = (*stack)->n;
+	if (num >= 0 || num <= 126)
+		putchar(num);
 	else
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
