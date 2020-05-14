2f2a55bd1fc0aff0bd475b4f4ce898cf98d412f2
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-14 14:25:13 -0500

    adding function _add.c

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 /* Free Double linked list */
 void free_dlistint(stack_t *stack);
 
