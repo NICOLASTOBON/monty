fd37d9d512db2ab65f9b5f052a1415bd8f485ae6
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-14 15:30:29 -0500

    add function mul

--- funcs3.c
+++ funcs3.c
@@ -33,13 +33,18 @@ void _div(stack_t **stack, unsigned int line_number)
  */
 void _mul(stack_t **stack, unsigned int line_number)
 {
+	stack_t *current = NULL;
+	int mul = 0;
+
 	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
+	current = *stack;
 
-	(*stack)->next->n *= (*stack)->n;
+	mul = current->next->n * current->n;
+	current->next->n = mul;
 	_pop(stack, line_number);
 }
 /**
