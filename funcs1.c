6c744a55e03d212b179ee6dc5eac161c3390ea79
melandres8 <melanmosta@gmail.com>
2020-05-14 04:33:45 +0000

    [Funcs] instructions

---
+++ funcs1.c
@@ -0,0 +1,87 @@
+#include "monty.h"
+
+/**
+ * new_Node - create new node
+ * @n: is a value
+ * Return: new node
+ */
+stack_t *new_Node(int n)
+{
+	stack_t *new = malloc(sizeof(stack_t));
+
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+
+/**
+ * _push - push item
+ * @stack: is a parameter
+ * @num: is value
+ */
+void _push(stack_t **stack, int num)
+{
+	stack_t *new = new_Node(num);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * _pall - print elements stack
+ * @stack: is a parameter
+ * @n: is value
+ * Return: nothing
+ */
+void _pall(stack_t **stack, unsigned int n)
+{
+	(void)n;
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+ * free_dlistint - Free a list.
+ * @stack: Head node.
+ * Return: Nothing.
+ */
+void free_dlistint(stack_t *stack)
+{
+	stack_t *current = stack;
+
+	if (current != NULL)
+	{
+		free_dlistint(current->next);
+		free(current);
+	}
+}
+
+/**
+ * _pint - prints the value at the top of the stack.
+ * @stack: Stack list
+ * @line_num: Number of the line
+ */
+void _pint(stack_t **stack, unsigned int line_num)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("%d\n", (*stack)->n);
+}
