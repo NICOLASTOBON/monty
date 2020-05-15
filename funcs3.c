4043d803fc6937bb40ddaaed13773d46befd84ac
melandres8 <melanmosta@gmail.com>
2020-05-15 03:14:24 +0000

    [changes] pchar instruction

--- funcs3.c
+++ funcs3.c
@@ -84,24 +84,22 @@ void _mod(stack_t **stack, unsigned int line_number)
  */
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	int num = 0;
-
-	if (!*stack || !stack)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		cleanStack(stack);
 		exit(EXIT_FAILURE);
 	}
 
-	num = (*stack)->n;
-	if (num < 0 || num > 127)
+	if ((*stack)->n > 0 && (*stack)->n < 127)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-		cleanStack(stack);
-		exit(EXIT_FAILURE);
+		putchar((*stack)->n);
+		putchar(10);
+		return;
 	}
-	putchar(num);
-	putchar(10);
+	fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+	cleanStack(stack);
+	exit(EXIT_FAILURE);
 }
 
 /**
